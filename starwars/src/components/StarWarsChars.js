31afcd7534c3514f2895006de3a16c82284600d1
ills5771 <ilyayelizarov1@gmail.com>
2019-02-15 09:17:19 -0800

    added some styling

--- starwars/src/components/StarWarsChars.js
+++ starwars/src/components/StarWarsChars.js
@@ -4,14 +4,17 @@ const StarWarsChars = props => {
   return (
     <div className="starwars-chars">
       {props.starwarsChars.map(char => (
-        <ul>
-          <li>Name: {char.name}</li>
-          <li>Gender:{char.gender}</li>
-          <li>Dob:{char.birth_year}</li>
-          <li>HairColor:{char.hair_color}</li>
-          <li>Weight:{char.mass}</li>
-          <li>Height:{char.height}</li>
-        </ul>
+        <div className="chars-card">
+          {" "}
+          <ul>
+            <li>Name: {char.name}</li>
+            <li>Gender:{char.gender}</li>
+            <li>Dob:{char.birth_year}</li>
+            <li>HairColor:{char.hair_color}</li>
+            <li>Weight:{char.mass}</li>
+            <li>Height:{char.height}</li>
+          </ul>{" "}
+        </div>
       ))}
     </div>
   );
